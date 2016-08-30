c0113786e050acc1976b977469821fbd364e2f78
mobbsy91 <6575@marlboroughcollege.org>
2016-08-30 14:24:10 +0100

    Created by Coding Ground Client

---
+++ New Project-20160830/main.py
@@ -0,0 +1,116 @@
+# name = Charlie
+# print name
+# for letter in name:
+#     print "Current letter is ", letter
+#     if letter == "r":
+#         break 
+    
+    
+# var = 100
+# while var <= 150:
+#     print "Number is ", var
+#     var = var + 1
+#     if var == 148:
+#         pass
+#         print "Pass break"
+ 
+import math
+import random
+
+# x = "variant"
+
+# list = ["test","cohice","damn"]
+# print random.choice(list)
+# print str.capitalize(x)
+
+
+# print "Hello %s, you are %d years old." % ("Charlie", 19)
+
+
+#list1 = ["test1", "test2", "test3"]
+#list2 = [1,2,3,4,5]
+
+#rint list1[0:2], list2[3:5]
+
+
+#list3 = [1,2,3,4,5,6]
+#list4 = [7,8,9]
+#print list3
+#del list3[3]
+#print list3
+#print list3 + list4
+#print len(list3)
+#print max(list3)
+#list3.append(2.5)
+#print list3
+#list3.append(2)
+#print list3
+#print list3.count(2)
+#list3.extend(list4)
+#print list3
+#list3.sort()
+#print list3
+
+#tup1 = (1,2,5,7)
+#tup2 = (3,4,6)
+
+#print tup1
+#tup3 = tup1 + tup2
+#print tup3
+#print len(tup3)
+
+#tup4 = tuple(list3)
+#print tup4
+
+
+import time
+import calendar
+
+#localtime = time.asctime(time.localtime())
+#print localtime
+
+
+
+#cal = calendar.month(2008,1)
+#print cal
+
+#def testfunc( namee ):
+#    print "Your name is", namee
+#    return;
+
+#def age(name, age, weight):
+#    print "Your name is", name
+#    print "Your age is", age
+#    print "Your weight is", weight,"kg"
+#    return;
+    
+#age(name="Charlie", age=19, weight=68)
+
+    
+#testfunc("Sarah")
+
+#print "test"
+
+#someinput = int(input("Test "))
+#print someinput
+
+
+#money = 2000
+
+#def addmoney():
+#    additional = int(input("How much would you like to add: "))
+#    global money
+#    money = money + additional
+    
+#print "Your current balance is", money
+#addmoney()
+#print "Your new balance is", money
+
+#print dir(calendar)
+#print dir(math)
+
+import os
+
+os.getcwd()
+
+import math
